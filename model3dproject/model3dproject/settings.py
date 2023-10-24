c5e16027ce856fb367d4fde3595be90ebb9291b3
amadoucodeur <amadoucodeur@gmail.com>
2023-10-24 16:31:47 +0000

    settings

--- model3dproject/model3dproject/settings.py
+++ model3dproject/model3dproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ufk-k$!2ytlvd7(lv%m_&h@w-tz_f7q9whs3$!1*v3cdf55)=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
